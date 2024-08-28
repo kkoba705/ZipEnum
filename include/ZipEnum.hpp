544b0f443c61a23f477e3c7424f5fc8ca28d7332
Keigo Kobayashi <kobayashi.keigo705@mail.kyutech.jp>
2024-08-28 19:01:58 +0900

    tuple

--- include/ZipEnum.hpp
+++ include/ZipEnum.hpp
@@ -60,66 +60,63 @@ inline auto zip(T1 && a, T2 && b) {
     return Zip<T1, T2>(std::forward<T1>(a), std::forward<T2>(b));
 }
 
-template<class T1, class T2, class T3>
-struct Zip3 {
-    T1 c1_;
-    T2 c2_;
-    T3 c3_;
 
-    using iterator1 = decltype(std::begin(c1_));
-    using iterator2 = decltype(std::begin(c2_));
-    using iterator3 = decltype(std::begin(c3_));
-    using terminator1 = decltype(std::end(c1_));
-    using terminator2 = decltype(std::end(c2_));
-    using terminator3 = decltype(std::end(c3_));
+template <typename ... A, typename ... E, std::size_t ... Index>
+inline bool iterator_cmp(std::tuple<A...> const& a, std::tuple<E...> const& end, 
+    std::index_sequence<Index...>) {
+    bool r = true;
+    r = (... && (std::get<Index>(a) != std::get<Index>(end)));
+    return r;
+}
 
-    Zip3(T1 && c1, T2 && c2, T3 && c3) :
-        c1_(std::forward<T1>(c1)), c2_(std::forward<T2>(c2)) , c3_(std::forward<T3>(c3)) {}
+template<typename ... T>
+struct ZipN {
+    std::tuple<T...> c_;
+
+    using iterators = std::tuple<decltype(std::begin(std::declval<T>())) ...>;
+    using terminators = std::tuple<decltype(std::end(std::declval<T>())) ...>;
+    using values = std::tuple<decltype(*std::begin(std::declval<T>())) ...>;
+
+    ZipN(T && ... c)  : c_{std::forward<T>(c) ...} {}
 
     struct terminator {
-        terminator1 t1_;
-        terminator2 t2_;
-        terminator3 t3_;
+        terminators t_;
     };
 
     struct iterator {
-        iterator1 i1_;
-        iterator2 i2_;
-        iterator3 i3_;
-
-        using V1 = decltype(*i1_);
-        using V2 = decltype(*i2_);
-        using V3 = decltype(*i3_);
+        iterators i_;
 
         bool operator!=(terminator const& a) const {
-            return (i1_ != a.t1_) && (i2_ != a.t2_) && (i3_ != a.t3_);
+            return iterator_cmp(i_, a.t_, std::index_sequence_for<T...>{});
         }
 
-        std::tuple<V1, V2, V3> operator*() const {
-            return {*i1_, *i2_, *i3_};
+        values operator*() const {
+            return std::apply([](auto && ... args){
+                return values(*args...);}, i_);
         }
 
-        iterator& operator++() {
-            ++i1_;
-            ++i2_;
-            ++i3_;
+        iterator& operator++() {std::apply([](auto && ... args){
+            ((++args), ...); }, i_);
             return *this;
         }
     };
 
-    auto begin() {
-        return iterator {std::begin(c1_), std::begin(c2_), std::begin(c3_)};
+    iterator begin() {
+        return {std::apply([](auto && ... args){
+            return iterators(std::begin(args)...);
+        }, c_)};
     }
 
-    auto end() {
-        return terminator {std::end(c1_), std::end(c2_), std::end(c3_)};
+    terminator end() {
+        return {std::apply([](auto && ... args){
+            return terminators(std::end(args)...);
+        }, c_)};
     }
 };
 
-template<class T1, class T2, class T3>
-inline auto zip(T1 && a, T2 && b, T3 && c) {
-    return Zip3<T1, T2, T3>(
-        std::forward<T1>(a), std::forward<T2>(b), std::forward<T3>(c));
+template<typename ... T>
+inline auto zip(T && ... t) {
+    return ZipN<T...> {std::forward<T>(t)...};
 }
 
 
@@ -164,65 +161,24 @@ struct Enumerate {
     }
 };
 
-template<class T1, class T2, class Int = int>
-struct Enumerate2 {
-    T1 c1_;
-    T2 c2_;
-
-    Enumerate2(T1 && c1, T2 && c2) : 
-        c1_(std::forward<T1>(c1)), c2_(std::forward<T2>(c2)) {}
-
-    using Iterator1 = decltype(std::begin(c1_));
-    using Iterator2 = decltype(std::begin(c2_));
-    using Terminator1 = decltype(std::end(c1_));
-    using Terminator2 = decltype(std::end(c2_));
-
-    struct terminator {
-        Terminator1 t1_;
-        Terminator2 t2_;
-    };
-
-    struct iterator {
-        Iterator1 i1_;
-        Iterator2 i2_;
-        Int c_ = 0;
-
-        using V1 = decltype(*i1_);
-        using V2 = decltype(*i2_);
-
-        bool operator!=(terminator const& a) const {
-            return (i1_ != a.t1_) && (i2_ != a.t2_);
-        }
-
-        auto operator*() const {
-            return std::tuple<int, V1, V2> {c_, *i1_, *i2_};
-        }
-
-        iterator& operator++() {
-            ++i1_;
-            ++i2_;
-            ++c_;
-            return *this;
-        }
-    };
-
-    auto begin() {
-        return iterator {std::begin(c1_), std::begin(c2_)};
-    }
-
-    auto end() {
-        return terminator {std::end(c1_), std::end(c2_)};
-    }
-};
-
 template<class T>
 inline auto enumerate(T && a) {
     return Enumerate<T>(std::forward<T>(a));
 }
 
-template<class T1, class T2>
-inline auto enumerate(T1 && a, T2 && b) {
-    return Enumerate2<T1, T2>(std::forward<T1>(a), std::forward<T2>(b));
+template<typename Int = int>
+struct counter {
+    Int c_ = 0;
+    auto begin() {return counter{0};}
+    auto end() {return counter{};}
+    auto operator*() const {return c_;}
+    void operator++() {++c_; }
+    bool operator!=(counter) const {return true;}
+};
+
+template<typename ... T>
+inline auto enumerate(T && ... t) {
+    return ZipN<counter<>, T...> {{}, std::forward<T>(t)...};
 }
 
 
